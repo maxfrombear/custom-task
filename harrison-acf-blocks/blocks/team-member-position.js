9c48a33d00b0452585c9eae95b35749109ea5a13
maxfrombear <140923928+maxfrombear@users.noreply.github.com>
2023-07-30 02:21:34 +0300

    Update team-member-position.js

--- harrison-acf-blocks/blocks/team-member-position.js
+++ harrison-acf-blocks/blocks/team-member-position.js
@@ -1,8 +1,6 @@
 (function (blocks, editor, components, data, compose) {
     var el = wp.element.createElement;
     var __ = wp.i18n.__;
-    var withSelect = wp.data.withSelect;
-    var dispatch = wp.data.dispatch;
     
 
     blocks.registerBlockType('harrison-acf-blocks/team-member-position', {
@@ -19,66 +17,23 @@
             typography: {fontSize: true,},
             anchor: true
         },
-        edit: compose.compose(
-            withSelect(function (select, props) {
-                var postId = select('core/editor').getCurrentPostId();
-                return {
-                    position: select('core/editor').getEditedPostAttribute('meta')['team_member_position'] || '',
-                    postId: postId,
-                };
-            }),
-            function (props) {
-                var position = props.position;
-        
-                function onChangePosition(newPosition) {
-                    dispatch('core/editor').editPost({ meta: { team_member_position: newPosition } });
-                }
-        
-                return el(components.Fragment, {}, [
-                    el(components.InspectorControls, {}, [
-                        el(components.PanelBody, {
-                            title: __('Block Settings', 'harrison-acf-blocks'),
-                            initialOpen: true,
-                        }, [
-                            el(components.PanelRow, {}, [
-                                el(components.ToggleControl, {
-                                    label: __('Align Left', 'harrison-acf-blocks'),
-                                    checked: props.attributes.align === 'left',
-                                    onChange: function (align) {
-                                        props.setAttributes({ align: align ? 'left' : undefined });
-                                    },
-                                }),
-                                el(components.ToggleControl, {
-                                    label: __('Align Center', 'harrison-acf-blocks'),
-                                    checked: props.attributes.align === 'center',
-                                    onChange: function (align) {
-                                        props.setAttributes({ align: align ? 'center' : undefined });
-                                    },
-                                }),
-                                el(components.ToggleControl, {
-                                    label: __('Align Right', 'harrison-acf-blocks'),
-                                    checked: props.attributes.align === 'right',
-                                    onChange: function (align) {
-                                        props.setAttributes({ align: align ? 'right' : undefined });
-                                    },
-                                }),
-                            ]),
-                        ]),
-                    ]),
-                    el(components.RichText, {
-                        tagName: 'p',
-                        value: position,
-                        onChange: onChangePosition,
-                        placeholder: __('Enter team member position...', 'harrison-acf-blocks'),
-                        style: { fontWeight: 'normal' },
-                        formattingControls: ['bold', 'italic', 'link', 'strikethrough'],
-                        isSelected: false,
-                        // Add 'className' to apply alignment dynamically
-                        className: props.attributes.align ? 'align-' + props.attributes.align : null,
-                    }),
-                ]);
+        edit: function (props) {
+            var position = props.attributes.position;
+
+            function onChangePosition(newPosition) {
+                // Update the 'position' attribute when content changes
+                props.setAttributes({ position: newPosition });
             }
-        ),
+
+            return el(blockEditor.RichText, {
+                tagName: 'p',
+                value: position,
+                onChange: onChangePosition,
+                placeholder: __('Enter team member position...', 'harrison-acf-blocks'),
+                style: { fontWeight: 'normal' },
+                formattingControls: ['bold', 'italic', 'link', 'strikethrough'],
+            });
+        },
         save: function (props) {
             return el(components.RichText.Content, {
                 tagName: 'p',
