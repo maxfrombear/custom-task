06ff97746c1c06fdd7e6e486e8b4a9b9c7875a9a
maxfrombear <140923928+maxfrombear@users.noreply.github.com>
2023-07-30 03:18:59 +0300

    Update harrison-acf-blocks.php

--- harrison-acf-blocks/harrison-acf-blocks.php
+++ harrison-acf-blocks/harrison-acf-blocks.php
@@ -12,28 +12,54 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-function harrison_acf_blocks_enqueue_scripts() {
-    wp_register_script(
-        'harrison-acf-blocks-team-member-position',
-        plugin_dir_url(__FILE__) . 'blocks/team-member-position/index.js',
-        array('wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-data', 'wp-i18n'),
-        filemtime(plugin_dir_path(__FILE__) . 'blocks/team-member-position/index.js')
-    );
+/*
+Plugin Name: Harrison ACF Blocks
+*/
 
-    wp_register_style(
-        'harrison-acf-blocks-team-member-position-style',
-        plugin_dir_url(__FILE__) . 'blocks/team-member-position/style.css',
-        array(),
-        filemtime(plugin_dir_path(__FILE__) . 'blocks/team-member-position/style.css')
-    );
 
-    // Enqueue the scripts and styles for the block
-    if (function_exists('register_block_type')) {
-        register_block_type('harrison-acf-blocks/team-member-position', array(
-            'editor_script' => 'harrison-acf-blocks-team-member-position',
-            'editor_style' => 'harrison-acf-blocks-team-member-position-style',
-            'style' => 'harrison-acf-blocks-team-member-position-style',
+// Register ACF block
+function harrison_acf_blocks_register_blocks() {
+    if (function_exists('acf_register_block')) {
+        acf_register_block(array(
+            'name' => 'team-member-position',
+            'title' => __('Team Member Position', 'harrison-acf-blocks'),
+            'description' => __('A custom block to display the "team_member_position" field.', 'harrison-acf-blocks'),
+            'render_callback' => 'harrison_acf_blocks_render_team_member_position_block',
+            'category' => 'common',
+            'icon' => 'admin-users',
+            'keywords' => array('team', 'position', 'acf'),
+            'supports' => array(
+                'align' => true,
+                'color' => array('background', 'text', 'gradients', 'link'),
+                'typography' => array('fontSize', 'lineHeight'),
+                'anchor' => true,
+                'spacing' => array('margin', 'padding', 'blockGap'),
+            ),
+            'enqueue_style' => plugin_dir_url(__FILE__) . 'blocks/team-member-position/style.css', // Optional: Add CSS file for block styles
+            'enqueue_script' => plugin_dir_url(__FILE__) . 'blocks/team-member-position/index.js', // Enqueue the block script
+            'post_types' => array('team-member'), // Specify the custom post type to which the ACF field group is assigned
         ));
     }
 }
-add_action('enqueue_block_editor_assets', 'harrison_acf_blocks_enqueue_scripts');
+add_action('acf/init', 'harrison_acf_blocks_register_blocks');
+
+function harrison_acf_blocks_render_team_member_position_block($block, $content = '', $is_preview = false, $post_id = 0) {
+    if (is_admin() && $is_preview) {
+        // Display a preview of the block in the Gutenberg editor
+        return '<p>' . __('Team Member Position Preview', 'harrison-acf-blocks') . '</p>';
+    } else {
+        // Check if the current post has the "team_member_position" field
+        $position = get_field('team_member_position', $post_id);
+
+        // Output the block content with formatting controls
+        if ($position) {
+            return '<div class="wp-block-team-member-position">' .
+                '<p>' . wp_kses_post($position) . '</p>' .
+                '</div>';
+        }
+
+        // If the "team_member_position" field is empty, return an empty string
+        return '';
+    }
+}
+
